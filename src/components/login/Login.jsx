333bf1563e1a30c6b5f66077d85f18bf0c8f7c34
clay.stup@gmail.com <clay.stup@gmail.com>
2018-01-06 20:34:35 -0800

    Finish up functionality of user login / registering

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -11,7 +11,8 @@ class Login extends React.Component {
         super(props);
         this.state = {
             username: '',
-            password: ''
+            password: '',
+            message: ''
         };
     }
     async login(){
